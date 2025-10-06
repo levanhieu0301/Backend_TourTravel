eea20a0d479a73c4b8cdb15cd4bafaa5cb91e473
lvh0301 <levanhieu0301@gmail.com>
2025-10-07 00:27:04 +0700

    Tích hợp Pug

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 7000;
+const path = require("path")
+
+// Tích hợp pug cho dự án
+app.set('views', path.join(__dirname, "views"))
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index',{
+    pageTitle: "Trang chủ"
+  })
 })
 
 app.listen(port, () => {
